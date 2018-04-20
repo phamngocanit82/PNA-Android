91835f49f094b8db6c212bc341db930be14b4c4f
ngocan.pham@ogilvy.com <ngocan.pham@ogilvy.com>
2018-04-20 10:44:21 +0700

    PNA-Android

---
+++ app/src/main/java/androidproject/pna/com/pnaandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package androidproject.pna.com.pnaandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
